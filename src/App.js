0068b07b613d2ed849b755dd924e1a2a8d2085af
aratane <akunbotak503@gmail.com>
2022-04-14 13:24:53 +0700

    up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+	return (
+		<ChatEngine
+			projectID='5461cf8c-1176-4324-883f-9613db04bfd7'
+			userName='sanji'
+			userSecret='admin123'
+		/>
+	);
+}
+
+export default App;
